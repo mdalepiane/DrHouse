d77f361633b54ba2ffffb6116cb6cc7373070e7a
Mateus Dalepiane <mdalepiane@stone.com.br>
2016-08-08 19:26:25 -0300

    Include Common.Logging support for core

--- src/DrHouse/HealthChecker.cs
+++ src/DrHouse/HealthChecker.cs
@@ -1,14 +1,15 @@
-﻿using System;
+﻿using Common.Logging;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DrHouse.Core
 {
     public class HealthChecker
     {
+        private readonly ILog _logger = LogManager.GetLogger<HealthChecker>();
+
         private readonly string _appName;
         private ICollection<IHealthDependency> _healthDependencyCollection;
 
@@ -20,11 +21,15 @@ namespace DrHouse.Core
 
         public void AddDependency(IHealthDependency dependency)
         {
+            _logger.Debug($"Adding health dependency to {_appName}.");
             _healthDependencyCollection.Add(dependency);
+            _logger.Debug($"Health dependency added to {_appName}.");
         }
 
         public HealthData CheckHealth()
         {
+            _logger.Debug($"Check health for {_appName} started.");
+
             HealthData healthData = new HealthData(_appName);
 
             try
@@ -36,14 +41,19 @@ namespace DrHouse.Core
                     healthDataCollection.Add(dep.CheckHealth());
                 });
 
+                _logger.Debug($"Check health completed for {_appName}.");
                 healthData.DependenciesStatus.AddRange(healthDataCollection);
                 healthData.IsOK = true;
             }
             catch (Exception ex)
             {
+                _logger.Error($"Check health crashed for {_appName}: {ex.Message}", ex);
+
                 healthData.IsOK = false;
                 healthData.ErrorMessage = "HealthChecker crashed.";
             }
+
+            _logger.Info($"Health check for {_appName} finished. Success: {healthData.IsOK}.");
 
             return healthData;
         }
